c4b997f4c12339eaaa1e07ecb16387258c9425db
Theifhawk <wchen291@gmail.com>
2025-05-08 14:42:46 -0400

    Showing metrics and status

---
+++ cfg.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	x := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", x)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset to 0"))
+}
--- main.go
+++ main.go
@@ -8,9 +8,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	handler := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	fmt.Println("Starting server at http://localhost:8080")
 	fmt.Println("Press Ctrl+C to stop the server")
